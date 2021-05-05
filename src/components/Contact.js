6375d5a4c606f83720d08f985cb2b2e9b7c3556d
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-05-05 13:18:32 +0100

    more content

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div>
+      <h2>Please contact me at the following..</h2>
+      <h2>LinkedIn - </h2>
+      <h2>Github - </h2>
+    </div>
+  )
+}
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Projects = () => {
+  return (
+    <div>
+      <h2>Here are some of my projects</h2>
+    </div>
+  )
+}
+
+export default Projects;
